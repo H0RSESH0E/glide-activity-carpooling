672fa9ac7bc13722a70681b80b1a302c5efc469c
Kaitlyn Skinner <kskinnerr@hotmail.com>
2022-04-07 15:27:07 -0400

    homeroutes working for homepage signup login dashboard - create vehicle create event and edit profile

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,8 +3,19 @@ const sequelize = require('../config/connection');
 const { Activity, User, Vehicle, Comment, Event, Location } = require('../models');
 const withAuth = require('../utils/auth');
 
+// http://localhost:3001/dashboard
 router.get('/', (req, res) => {
     res.render('dashboard');
+});
+
+// http://localhost:3001/create-vehicle
+router.get('/create-vehicle', (req, res) => {
+    res.render('create-vehicle');
+});
+
+//http://localhost:3001/create-event
+router.get('/createevent', (req, res) => {
+    res.render('createevent');
 });
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,32 +1,74 @@
 const router = require('express').Router();
+const authenticatedUser = require("../utils/auth.js");
 const sequelize = require('../config/connection');
 const res = require('express/lib/response');
 const path = require('path');
 const { Activity, User, Vehicle, Comment, Event, Location, Participant } = require('../models');
 
-// GET Method for all activities
-router.get('/activities', (req, res) => {
-  console.log('======================');
-  Activity.findAllUser.findAll({ include: [{ all: true, nested: true }]
-      })
-      .then(dbActivityData => res.json(dbActivityData))
-      
-      
-      res.render('activties', {
-        activities,
-        loggedIn: req.session.loggedIn
-      })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-    // path for /login, if loggedIn 
-  router.get('/login', (req, res) => {
-    res.render('login');
-  });
+router.get("/", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/homepage");
+  } else {
+    res.render('signup', {js: ['signup.handlebars']});
+  }
 });
 
-// login.html - login page
+// GET method for authenticated user to homepage
+router.get('/homepage', authenticatedUser, (req, res) => {
+  res.render('homepage', {js: ['homepage.handlebars']});
+});
+
+// // GET method to popular activities
+// router.get('/popular-activities', authenticatedUser, (req, res) => {
+//   if (req.session.loggedIn) {
+//     res.render('popular-activities', { js: ['popular-activities.handlebars']});
+//   } 
+//   // else {
+//   //   res.render('login', {js: ['login.handlebars']});
+//   // }
+// });
+
+// GET method to single event
+router.get('/single-event', authenticatedUser, (req, res) => {
+  if (req.session.loggedIn) {
+    res.render('single-event', { js: ['single-event.handlebars']});
+  } 
+  // else {
+  //   res.render('login', {js: ['login.handlebars']});
+  // }
+});
+
+// GET method to create vehicle
+router.get('/create-vehicle', authenticatedUser, (req, res) => {
+  if (req.session.loggedIn) {
+    res.render('create-vehicle', { js: ['create-vehicle.handlebars']});
+  } 
+  // else {
+  //   res.render('login', {js: ['login.handlebars']});
+  // }
+});
+
+// GET method to create events
+router.get('/create-event', authenticatedUser, (req, res) => {
+  if (req.session.loggedIn) {
+    res.render('create-event', { js: ['create-event.handlebars']});
+  } 
+  // else {
+  //   res.render('login', {js: ['login.handlebars']});
+  // }
+});
+
+// GET method to profile edit
+router.get('/profile-edit', authenticatedUser, (req, res) => {
+  if (req.session.loggedIn) {
+    res.render('profile-edit', { js: ['profile-edit.handlebars']});
+  } 
+  // else {
+  //   res.render('login', {js: ['login.handlebars']});
+  // }
+});
+
+// GET metho to login page
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/dashboard');
@@ -36,6 +78,7 @@ router.get('/login', (req, res) => {
     res.render('login')
 });
 
+// GET method to signup page
 router.get('/signup', (req, res) => {
     res.render('signup');
 });
