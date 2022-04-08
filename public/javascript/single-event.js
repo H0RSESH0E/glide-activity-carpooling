1522fd5a99cf22547be5df69fd5a9d1630e74e0f
Kaitlyn Skinner <kskinnerr@hotmail.com>
2022-04-08 14:59:25 -0400

    editted styles on pop act and sing event pages

--- public/javascript/single-event.js
+++ public/javascript/single-event.js
@@ -33,22 +33,8 @@ async function eventNameEl(e) {
     }
 }
 
+
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-
-// GOOGLE MAPS 
-function initMap() {
-    var test= {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: test
-    });
-    var marker = new google.maps.Marker({
-      position: test,
-      map: map
-    });
-}
-
-
 // if (eventNa, EventDe, EventBeTime, eventEnTime, eventMaxPar, EventMinPar) {
 //     const response = await fetch('/api/event', {
 //         method: 'POST',
